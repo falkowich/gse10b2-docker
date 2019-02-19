104880d26ef2aa209d2ce6263d77e8deaf070321
Falk <andreas.alk@nykoping.se>
2019-02-19 12:18:34 +0100

    working psql

--- script/postgresqlconf.sh
+++ script/postgresqlconf.sh
@@ -2,10 +2,9 @@
 
 service postgresql restart ;\
 su postgres sh -c "createuser -DRS root " ;\
-su postgres sh -c "createdb -O root gvmd"
-
+su postgres sh -c "createdb -O root gvmd" ;\
 su postgres sh -c "psql -d gvmd"   << EOSQL
-    create role dbas with superuser noinherit;
-    grant dbas to root;
+    create role dba with superuser noinherit;
+    grant dba to root;
     CREATE EXTENSION "uuid-ossp";
 EOSQL
